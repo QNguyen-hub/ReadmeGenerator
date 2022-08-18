199e22b69ff2ecc1390b3459f7e30a9d7ebc15ba
Quy Nguyen <85922363+QNguyen-hub@users.noreply.github.com>
2022-08-17 22:46:08 -0600

    added table of content with link

--- index.js
+++ index.js
@@ -52,18 +52,27 @@ inquirer
   .then((userInput) =>
     fs.writeFile(
       "README.MD",
-      `<h># ${userInput.projectname}</h1>
+      `<h1># ${userInput.projectname}</h1>
       <p>This project is about ${userInput.about}</p>
-      <h2># Languages</h2>
+      <h2>Table of Content</h2>
+      <h3><ul>
+      <li><a href="#languages">Languages</a></li>
+      <li><a href="#npm">NPM</a></li>
+      <li><a href="#challenges">Challenges</a></li>
+      <li><a href="#prospect">Procpect</a></li>
+      <li><a href="#screenshot">Screenshot/Video</a></li>
+      <li><a href="#git">GitHub</a></li>
+      </ul></h3>
+      <h2 id="languages"># Languages</h2>
       <p> This project uses these languages ${userInput.languages}</p>
-      <h2># NPM</h2>
+      <h2 id="npm"># NPM</h2>
       <p> This project utilizes these NPM ${userInput.npm}
-      <h2># Challenges</h2>
+      <h2 id="challenges"># Challenges</h2>
       <p> Some of the challenges of this projects are: ${userInput.issues}</p>
-      <h2># Prospect</h2>
+      <h2 id="prospect"># Prospect</h2>
       <p> Future prospect or development of this project are: ${userInput.prospect}</p>
-      <h2># Screenshot or Video</h2>
-      <h2># GitHub Pages</h2>
+      <h2 id="screeenshot"># Screenshot or Video</h2>
+      <h2 id="git"># GitHub Pages</h2>
       <a href=${userInput.pages}>View the GitHub Pages</a>
       <h2># GitHub Repository</h2>
       <a href=${userInput.repository}>View the GitHub Repository</a>`,
